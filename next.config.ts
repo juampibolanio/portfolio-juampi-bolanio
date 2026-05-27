0d5d48980f1aba9316b98314354bce7eaa7a350e
JuampiBolanio <juampi360786@gmail.com>
2026-05-27 14:26:07 -0300

    chore: habilitar origenes para imagenes de prueba

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname,
-  }
+  },
+  images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com', 
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
