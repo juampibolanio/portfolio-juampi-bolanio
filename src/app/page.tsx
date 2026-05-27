389e39dfbc416edc19347f5c021d9a39a811fdf8
JuampiBolanio <juampi360786@gmail.com>
2026-05-27 14:29:01 -0300

    feat: agregar al home la seccion de proyectos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import AboutMe from "./components/AboutMe";
 import Experience from "./components/Experience";
 import Hero from "./components/Hero";
+import Projects from "./components/Projects";
 import Stack from "./components/Stack";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <AboutMe />
       <Experience />
       <Stack />
+      <Projects />
     </main>
   );
 }
