e161332ff89fe048ebb1550aa947930bfc710833
JuampiBolanio <juampi360786@gmail.com>
2026-06-26 12:18:07 -0300

    feat: actualizar patrón remoto de imágenes a Cloudinary en la configuración de Next.js

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'images.unsplash.com', 
+                hostname: 'res.cloudinary.com', 
                 port: '',
                 pathname: '/**',
             },
