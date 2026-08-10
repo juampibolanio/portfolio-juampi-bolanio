4664534952a1fd6eaa45145f2d61d467112aaf38
Juan Pablo Bolanio <juampi360786@gmail.com>
2026-08-10 16:24:48 -0300

    feat(technologies): crear servicio para gestion de tecnologias

--- src/features/technologies/services/technologies.service.ts
+++ src/features/technologies/services/technologies.service.ts
@@ -1,8 +1,21 @@
 import { fetchApi } from "@/common/api/api";
-import { Technology } from "@/features/technologies/interfaces/technology.interface";
+import { Technology, CreateTechnologyInput } from "../interfaces/technology.interface";
 
 export const technologyService = {
   getAll: async () => {
     return fetchApi<Technology[]>('/technologies');
+  },
+  
+  create: async (data: CreateTechnologyInput) => {
+    return fetchApi<Technology>('/technologies', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    });
+  },
+
+  delete: async (uuid: string) => {
+    return fetchApi(`/technologies/${uuid}`, {
+      method: 'DELETE',
+    });
   }
 };
